1e7363c95e22aaecc33f25172102f92112f8cbd5
Lalit C <prod8ctive.analytics@gmail.com>
2022-02-13 19:37:07 -0600

    Implemented AWS LoadBalancer based Kubernetes service

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,12 +1,57 @@
 import { Construct } from 'constructs';
 import { App, Chart, ChartProps } from 'cdk8s';
 
+// imported constructs
+import { KubeDeployment, KubeService, IntOrString } from './imports/k8s';
+
+/**
+ * CDK8S application 
+ *    with simple webserver k8s nodes deployed on AWS EKS,
+ *    front-ended by AWS LoadBalancer service
+ * 
+ * Author: ProductiveAnalytics
+ */
 export class MyChart extends Chart {
+
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
     super(scope, id, props);
 
-    // define resources here
+    // Refer: https://hub.docker.com/r/paulbouwer/hello-kubernetes/
+    const DOCKER_IMG_VERSION = '1.10.1';
+    const PREFIX = 'cdk8s-ts-on-eks';
+    const label = { app: PREFIX }
 
+    // define resources here
+    const CDK_K8S_SERVICE_ID = (PREFIX + '-svc')
+    new KubeService(this, CDK_K8S_SERVICE_ID, {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    })
+
+    const CDK_K8S_DEPLOYMENT_ID = (PREFIX + '-deployment')
+    new KubeDeployment(this, CDK_K8S_DEPLOYMENT_ID, {
+      spec: {
+        selector: {
+          matchLabels: label
+        },
+        replicas: 3,
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:'+DOCKER_IMG_VERSION,
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    })
   }
 }
 
